91a14583ef274fadfc00d52972d38340dd8702b1
Artem <artctv@outlook.com>
2020-10-15 22:37:58 +0200

    Update Sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,8 +27,7 @@ author = 'Artem'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['recommonmark',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -57,3 +56,11 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# Configures Sphinx to parse all files with the extensions .md and .txt as Markdown and .rst as restructuredtext:
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
